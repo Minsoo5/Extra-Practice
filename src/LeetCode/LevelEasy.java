0afb120eb8f51b60b19c74b095e676b9ca4c4e81
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-09-22 09:21:04 -0400

    LeetCode LevelEasy

--- src/LeetCode/LevelEasy.java
+++ src/LeetCode/LevelEasy.java
@@ -1,5 +1,28 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LevelEasy {
+
+
+        public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+            List<Boolean> ans = new ArrayList<>();
+            int biggestNum = 0;
+            for (int i = 0; i < candies.length; i++) {
+                if (candies[i] >= biggestNum) {
+                    biggestNum = candies[i];
+                }
+            }
+
+            for (int i = 0; i < candies.length; i++) {
+                if (candies[i] + extraCandies >= biggestNum) {
+                    ans.add(i, true);
+                } else {
+                    ans.add(i, false);
+                }
+            }
+            return ans;
+        }
 
 }
